1f5e24a735a491214f0db80ac285285c1d8e16dc
rainoceantop <rainoceantop@outlook.com>
2018-09-29 01:06:04 +0800

    ---

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .styles([
+        'node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+    ], 'public/css/fontawesome.all.css')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copy('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts');
